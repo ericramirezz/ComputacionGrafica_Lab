9e658d8aaaba7f50c72cdffb0f500d689abe04ee
ericramirez <143150337+ericramirezz@users.noreply.github.com>
2026-02-14 21:25:45 -0600

    Practica2_avance
    Avance con las coordenadas del dibujo de la cabra, ya esta el contorno

--- Prac1ConfigInicial/Main_Dibujo2D.cpp
+++ Prac1ConfigInicial/Main_Dibujo2D.cpp
@@ -1,3 +1,8 @@
+// Práctica 2: Dibujo de Primitivas en 2D
+// Autor: Eric Ramírez Valdovinos
+// Fecha: 15/02/2026
+// Descripción: Usando diferentes primitivas de OpenGL buscando dibujar una imagen con los puntos dados.
+ 
 #include<iostream>
 
 //#define GLEW_STATIC
@@ -23,7 +28,7 @@ int main() {
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);*/
 
-	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Dibujo de Primitivas en 2D", NULL, NULL);
+	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Dibujo de cabra con primitivas en 2D - Eric Ramírez", NULL, NULL);
 	glfwSetFramebufferSizeCallback(window, resize);
 	
 	//Verificaci�n de errores de creacion  ventana
@@ -59,14 +64,108 @@ int main() {
 
 	// Set up vertex data (and buffer(s)) and attribute pointers
 	float vertices[] = {
-		0.5f,  0.5f, 0.0f,    0.0f,1.0f,1.0f,  // top right
-		0.5f, -0.5f, 0.0f,    1.0f,1.0f,1.0f,  // bottom right
-		-0.5f, -0.5f, 0.0f,   1.0f,0.0f,1.0f,  // bottom left
-		-0.5f,  0.5f, 0.0f,   1.0f,1.0f,0.0f, // top left 
+		// ===== PATA TRASERA =====
+		-0.84f, -0.88f, 0.0f,   1.0f, 0.0f, 0.5f, // 0  Pezuña trasera inicio
+		-0.89f, -0.75f, 0.0f,   1.0f, 0.5f, 0.0f, // 1  Pezuña trasera final
+		-0.86f, -0.68f, 0.0f,   1.0f, 0.3f, 0.3f, // 2  Talón trasero
+		-0.80f, -0.60f, 0.0f,   0.8f, 0.2f, 0.6f, // 3  Rodilla trasera
+		-0.76f, -0.56f, 0.0f,   1.0f, 1.0f, 0.0f, // 4  Inicio cola
+		-0.71f, -0.44f, 0.0f,   1.0f, 0.4f, 0.5f, // 5  Cadera
+		-0.68f, -0.35f, 0.0f,   0.7f, 0.8f, 0.3f, // 6  Trasero superior
+		// COLA
+		-0.6f, -0.3f, 0.0f,   0.2f, 1.0f, 0.5f, // 7  Espalda baja
+		-0.55f, -0.25f, 0.0f,   0.3f, 0.9f, 0.4f, // 8  Espalda media
+		-0.51f, -0.14f, 0.0f,   1.0f, 0.5f, 0.0f, // 9  Hombro
+		-0.50f, -0.05f, 0.0f,   1.0f, 0.0f, 1.0f, // 10  Base cabeza
+		-0.55f,  0.00f, 0.0f,   1.0f, 0.2f, 0.6f, // 11 Mejilla
+		-0.59f,  0.08f, 0.0f,   1.0f, 0.6f, 0.2f, // 12 Frente
+		-0.50f,  0.10f, 0.0f,   1.0f, 0.5f, 0.5f, // 13 Hocico superior
+		-0.40f,  0.10f, 0.0f,   1.0f, 1.0f, 0.0f, // 14 Punta hocico
+		-0.30f, 0.11f, 0.0f,   0.5f, 0.0f, 1.0f, // 15 Barbilla
+		-0.20f,  0.14f, 0.0f,   1.0f, 0.0f, 0.0f, // 16 Cuello frontal
+		-0.10f, 0.18f, 0.0f,   0.8f, 0.8f, 0.8f, // 17 Cuello superior
+		-0.01f, 0.2f, 0.0f,   1.0f, 0.5f, 0.5f, // 18 Cuello final
+		0.15f, 0.3f, 0.0f,   1.0f, 0.5f, 0.5f, // 19
+		0.25f, 0.35f, 0.0f,   1.0f, 0.3f, 0.7f, // 20
+		0.33f, 0.41f, 0.0f,   0.6f, 0.2f, 0.8f, // 21
+		0.40f, 0.45f, 0.0f,   1.0f, 0.7f, 0.3f, // 22
+		0.47f, 0.47f, 0.0f,   0.7f, 0.7f, 0.2f, // 23
+		0.57f, 0.49f, 0.0f,   1.0f, 0.4f, 0.4f, // 24
+		0.58, 0.55f, 0.0f,   1.0f, 0.8f, 0.2f, // 25
+		0.59f, 0.55f, 0.0f,   1.0f, 0.9f, 0.1f, // 26
+		0.63f, 0.51f, 0.0f,   1.0f, 0.6f, 0.3f, // 27
+		0.63f, 0.56f, 0.0f,   1.0f, 0.7f, 0.2f, // 28
+		0.60f, 0.64f, 0.0f,   1.0f, 0.3f, 0.9f, // 29
+		0.56f, 0.70f, 0.0f,   1.0f, 0.5f, 0.7f, // 30
+		0.51f, 0.72f, 0.0f,   1.0f, 0.7f, 0.5f, // 31
+		0.42f, 0.76f, 0.0f,   1.0f, 0.8f, 0.4f, // 32
+		0.35f, 0.71f, 0.0f,   1.0f, 0.4f, 0.8f, // 33
+		0.27f, 0.6f, 0.0f,   1.0f, 0.2f, 0.8f, // 34
+		0.29f, 0.76f, 0.0f,   1.0f, 0.9f, 0.3f, // 35
+		0.30f, 0.80f, 0.0f,   1.0f, 0.5f, 0.5f, // 36
+		0.34f, 0.84f, 0.0f,   1.0f, 0.6f, 0.4f, // 37
+		0.4f, 0.89f, 0.0f,   1.0f, 0.7f, 0.3f, // 38
+		0.42f, 0.90f, 0.0f,   1.0f, 0.8f, 0.2f, // 39	
+		0.5f, 0.90f, 0.0f,   1.0f, 0.9f, 0.1f, // 40
+		0.6f, 0.88f, 0.0f,   1.0f, 0.6f, 0.4f, // 41
+		0.65f,0.85f, 0.0f,   1.0f, 0.4f, 0.6f, // 42
+		0.70f, 0.77f, 0.0f,   1.0f, 0.2f, 0.2f, // 43 Curva externa
+		0.75f, 0.7f, 0.0f,   1.0f, 0.3f, 0.7f, // 44 Curva externa
+		0.79f, 0.6f, 0.0f,   1.0f, 0.5f, 0.5f, // 45 Curva externa
+		0.81f, 0.51f, 0.0f,   1.0f, 0.6f, 0.4f, // 46
+		0.80f, 0.45f, 0.0f,   1.0f, 0.7f, 0.3f, // 47
+		0.81f, 0.36f, 0.0f,   1.0f, 0.8f, 0.2f, // 48
+		0.82f, 0.30f, 0.0f,   1.0f, 0.9f, 0.1f, // 49
+		0.84f, 0.26f, 0.0f,   1.0f, 0.5f, 0.5f, // 50
+		0.80f, 0.20f, 0.0f,   1.0f, 0.6f, 0.4f, // 51
+		0.79f, 0.14f, 0.0f,   1.0f, 0.7f, 0.3f, // 52
+		0.75f, 0.06f, 0.0f,   1.0f, 0.8f, 0.2f, // 53
+		0.74f, 0.10f, 0.0f,   1.0f, 0.9f, 0.1f, // 54
+		0.75f, -0.02f, 0.0f,   1.0f, 0.5f, 0.5f, // 55
+		0.73f, -0.05f, 0.0f,   1.0f, 0.6f, 0.4f, // 56
+		0.71f, -0.07f, 0.0f,   1.0f, 0.7f, 0.3f, // 57
+		0.68f, -0.06f, 0.0f,   1.0f, 0.8f, 0.2f, // 58
+		0.61f, 0.03f, 0.0f,   1.0f, 0.9f, 0.1f, // 59
+		0.52f, 0.02f, 0.0f,   1.0f, 0.5f, 0.5f, // 60
+		0.54f, 0.09f, 0.0f,   1.0f, 0.6f, 0.4f, // 61
+		0.55f, 0.14f, 0.0f,   1.0f, 0.7f, 0.3f, // 62
+		0.59f, 0.16f, 0.0f,   1.0f, 0.8f, 0.2f, // 63
+		0.55f, 0.18f, 0.0f,   1.0f, 0.9f, 0.1f, // 64
+		0.54f, 0.14f, 0.0f,   1.0f, 0.5f, 0.5f, // 65
+		0.50f, 0.0f, 0.0f,   1.0f, 0.6f, 0.4f, // 66
+		0.41f, -0.21f, 0.0f,   1.0f, 0.7f, 0.3f, // 67
+		0.5, -0.38f, 0.0f,   1.0f, 0.8f, 0.2f, // 68
+		0.4, -0.5f, 0.0f,   1.0f, 0.9f, 0.1f, // 69
+		0.3, -0.6f, 0.0f,   1.0f, 0.5f, 0.5f, // 70
+		0.2, -0.7f, 0.0f,   1.0f, 0.6f, 0.4f, // 71
+		0.18, -0.65f, 0.0f,   1.0f, 0.7f, 0.3f, // 72
+		0.15, -0.60f, 0.0f,   1.0f, 0.8f, 0.2f, // 73
+		0.20, -0.54f, 0.0f,   1.0f, 0.9f, 0.1f, // 74
+		0.21, -0.50f, 0.0f,   1.0f, 0.5f, 0.5f, // 75
+		0.3f, -0.48f, 0.0f,   1.0f, 0.6f, 0.4f, // 76
+		0.38f, -0.40f, 0.0f,   1.0f, 0.7f, 0.3f, // 77
+		0.28f, -0.38f, 0.0f,   1.0f, 0.8f, 0.2f, // 78
+		0.22f, -0.37f, 0.0f,   1.0f, 0.9f, 0.1f, // 79
+		0.12f, -0.30f, 0.0f,   1.0f, 0.5f, 0.5f, // 80
+		0.0f, -0.29f, 0.0f,   1.0f, 0.6f, 0.4f, // 81
+		-0.15f, -0.28f, 0.0f,   1.0f, 0.7f, 0.3f, // 82
+		-0.35f, -0.40f, 0.0f,   1.0f, 0.8f, 0.2f, // 83
+		-0.6f, -0.48f, 0.0f,   1.0f, 0.9f, 0.1f, // 84
+		-0.69f, -0.60f, 0.0f,   1.0f, 0.5f, 0.5f, // 85
+		-.8f, -.8f, 0.0f,   1.0f, 0.6f, 0.4f, // 86 Pata trasera final (cerca de la inicial para cerrar forma)
+
+
+
+
 	};
+
 	unsigned int indices[] = {  // note that we start from 0!
-		3,2,1,// second Triangle
-		0,1,3,
+		// Delineado del contorno (Silhouette)
+		0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 
+		28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
+		48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67,
+		68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 0
+		// Nota: El último conecta con el principio o cerca de él para cerrar formas
 		
 	};
 
@@ -120,17 +219,27 @@ int main() {
         glBindVertexArray(VAO);
 
 
-        glPointSize(10); // tamaño del punto
-        glDrawArrays(GL_POINTS,0,4); // dibujar  puntos
+        //glPointSize(10); // tamaño del punto
+        //glDrawArrays(GL_POINTS,0,4); // dibujar  puntos
 		//glDrawArrays(GL_POINTS, 2,1); // dibujar  puntos
         
         //glDrawArrays(GL_LINES,2,3); //unir puntos
 		//glDrawArrays(GL_LINE_LOOP, 0, 4); //unir lineas desde el punto x al ultimo punto y
-		glDrawArrays(GL_TRIANGLES, 1, 3); //dibujar triangulo con los puntos 1,2,3
-        //glDrawElements(GL_TRIANGLES, 3,GL_UNSIGNED_INT,0); //dibujar triangulo con los puntos 1,2,3 usando indices
+		//glDrawArrays(GL_TRIANGLES, 1, 3); //dibujar triangulo con los puntos 1,2,3
+        //glDrawElements(GL_TRIANGLES, 6,GL_UNSIGNED_INT,0); //dibujar triangulo con los puntos 1,2,3 usando indices
 
-        
-        
+		// Opcional: Aumentar grosor de línea para estilo "neon"
+		glLineWidth(2.0f);
+		glPointSize(10.0f); // Para ver los vertices como puntos guía
+
+		// 1. Dibujar los Puntos (Para que veas dónde caen las coordenadas)
+		//glDrawArrays(GL_POINTS, 0, 87);
+
+		// 2. Dibujar las Líneas conectando los puntos (Usando los indices del EBO)
+		// Nota: Como estamos usando indices, usamos glDrawElements
+		// GL_LINE_STRIP: Une punto A con B, B con C...
+		glDrawElements(GL_LINE_STRIP, 88, GL_UNSIGNED_INT, 0);
+
         glBindVertexArray(0);
     
 		// Swap the screen buffers
